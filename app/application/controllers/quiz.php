fba5944783913db25e563b0c5fd31b415b66235e
Shimanyi <vshimanyi@gmail.com>
2015-03-03 18:30:36 +0300

    fixed function on the controller

--- app/application/controllers/quiz.php
+++ app/application/controllers/quiz.php
@@ -12,23 +12,38 @@ class Quiz extends CI_Controller {
 
 	public function index()
 	{
-		$this->getQuestion(1);
+		$this->(1);
 	}
-	public function reg_user(){
-		$data['phone'] =  '+254720255774';
-		$data['name']  =  'shimanyi';
+	public function recv_sms($phone,$msg,$time){
+		#system access point
+		if($this->_no_such_user($phone)){
+			$this->reg_user($phone,$msg,$time);
+		} else {
+
+			#answer validations
+			$result = $this->quiz_model->validate($phone, $msg, $time);
+			if($result){
+				#update quiz_count in the db
+				$res = $this->quiz_model->update_usr($phone);
+				if($res){
+					$this->sendQue($phone);
+				} else {
+					#only magic can get you here XD
+				}	
+
+			} else {
+				#wrong answer was submitted
+				$this->sendQue($phone);
+			}
+		}
+	}
+	public function reg_user($phone, $name){
+		$data['phone'] =  $phone;
+		$data['name']  =  $name;
 		$data['time']  =  date('y-m-d h:m:s');
 		$data['quiz_count'] = 1;
 
-		if($this->_no_such_user($data['phone'])){
-			$this->quiz_model->reg_user($data);
-		} 
-
-
-		else{
-			$usr_count = userCount($data['phone']);
-			$this->sendNextQue($data['phone'], $usr_count);	
-		}	
+		$this->quiz_model->reg_user($data);	
 	}
 	public function _no_such_user($phone){
 		return $this->quiz_model->is_available($phone);
@@ -39,14 +54,7 @@ class Quiz extends CI_Controller {
 			$quiz_count = $row->quiz_count;
 		}
 		return $quiz_count;
-	}
-
-	public function sendNextQue($phone, $question){
-
 	}	
-	public function flagFail(){
-		
-	}
 	public function getQuestion($que_num){
 
 		$result = $this->quiz_model->getQuestion($que_num);
@@ -57,6 +65,15 @@ class Quiz extends CI_Controller {
 
 		return $question;
 	}
-}
+	public function sendQue($phone){
+		$user_count = $this->userCount($phone);
+		$que = $this->getQuestion($user_count);
 
+		$toSend = array(
+			'phone'   => $phone,
+			'message' => $que
+			);
+		return $toSend;
+	}
+}
 /* End of file quiz.php */
