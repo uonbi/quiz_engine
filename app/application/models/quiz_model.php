1def04bde4a23776cd2c52e4e123dfe1f045a72c
shimanyi <vshimanyi@gmail.com>
2015-03-07 10:53:31 +0300

    fixed probation bug

--- app/application/models/quiz_model.php
+++ app/application/models/quiz_model.php
@@ -206,7 +206,7 @@ class Quiz_model extends CI_Model {
 	public function redeem_module($var, $phone){
 		print_r($var);
 		$result = $this->db->query("SELECT * FROM redemptions INNER JOIN members ON members.redeem_quest = redemptions.owner
-						WHERE redemptions.owner = '$var' AND members.member_id = redemptions.member_id AND members.phone='$phone' LIMIT 0 , 30");
+						WHERE redemptions.owner = '$var' AND members.phone='$phone' LIMIT 0 , 30");
 		if($this->db->affected_rows() != 0){
 			return true;
 		} else {
